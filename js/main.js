63828ac0d9644e4e60e2efff41eec497bca6c0a4
Thomas2212 <121277715+thomasrequeno@users.noreply.github.com>
2022-12-26 18:07:19 -0600

    Actualizacion de css y refactorizacion

--- js/main.js
+++ js/main.js
@@ -1,45 +1,96 @@
-var diccionario =
-    {
-        e: "enter",
-        i: "imes",
-        a: "ai",
-        o: "ober",
-        u: "ufat"
-    };
+var diccionario = {
+  e: "enter",
+  i: "imes",
+  a: "ai",
+  o: "ober",
+  u: "ufat",
+};
 var encrypt = document.querySelector("#encrypt");
 var desencrypt = document.querySelector("#desencrypt");
 var auto = document.querySelector("#auto");
+var copyInfo = document.querySelector("#copy");
 
+/*
+ * Funcion encriptar
+ * texto (string)
+ * return @result (encript)
+ */
 function encriptar(texto) {
-    var result = texto;
-    for (const [key, value] of Object.entries(diccionario))
-        result = result.replaceAll(key, value);
-    return result;
+  var result = texto;
+  for (const [key, value] of Object.entries(diccionario))
+    result = result.replaceAll(key, value);
+  return result;
 }
+/*
+ * Funcion desencriptar
+ * texto (string)
+ * return @result (desencript)
+ */
 function desencriptar(texto) {
-    var result = texto;
-    for (const [key, value] of Object.entries(diccionario))
-        result = result.replaceAll(value, key);
+  var result = texto;
+  for (const [key, value] of Object.entries(diccionario))
+    result = result.replaceAll(value, key);
 
-    return result;
+  return result;
 }
 
+/*
+ * Funcion getTexto
+ * tipo (integer) [Tipo de accion: 1 encriptar, 2 desencryptar]
+ * return @result (desencript)
+ */
 function getTexto(tipo) {
-    if (tipo == 1) desencrypt.value = encriptar(encrypt.value);
-    else if (tipo == 2) desencrypt.value = desencriptar(encrypt.value);
+  if (tipo == 1) desencrypt.value = encriptar(encrypt.value);
+  else if (tipo == 2) desencrypt.value = desencriptar(encrypt.value);
+
+  changeBackground();
 }
-function getEncrypt()
-{
-    this.value = this.value.toLowerCase()
-    if(auto.checked)
-        desencrypt.value = encriptar(encrypt.value);
+
+/*
+ * Funcion changeBackground
+ * [Cambia el background del textarea desencryptar]
+ */
+function changeBackground() {
+  if (desencrypt.value.length > 0)
+    desencrypt.classList.replace("imagen", "blanco");
+  else desencrypt.classList.replace("blanco", "imagen");
 }
-function getDesencrypt(){
-    if (auto.checked)
-        encrypt.value = desencriptar(desencrypt.value);
+/*
+ * Funcion getEncrypt
+ * [Listener para encriptar de forma automatica]
+ */
+function getEncrypt() {
+  this.value = this.value.toLowerCase();
+  if (auto.checked) desencrypt.value = encriptar(encrypt.value);
+  changeBackground();
 }
+/*
+ * Funcion getDesencrypt
+ * [Listener para desencriptar de forma automatica]
+ */
+function getDesencrypt() {
+  if (auto.checked) encrypt.value = desencriptar(desencrypt.value);
+  changeBackground();
+}
+
+/*
+ * Funcion copy
+ * [Copia el contenido del textarea desencriptar al clipboard]
+ */
 function copy() {
-    navigator.clipboard.writeText(desencrypt.value)
+  if (desencrypt.value.length > 0) {
+    navigator.clipboard.writeText(desencrypt.value);
+    copyInfo.classList.replace("danger", "info");
+    copyInfo.innerHTML = "Copy";
+  } else {
+    copyInfo.classList.replace("info", "danger");
+    copyInfo.innerHTML = "¡Nada para copiar!";
+  }
+
+  copyInfo.style.display = "block";
+  setInterval(() => {
+    copyInfo.style.display = "none";
+  }, 2500);
 }
 
 encrypt.onkeyup = getEncrypt;
